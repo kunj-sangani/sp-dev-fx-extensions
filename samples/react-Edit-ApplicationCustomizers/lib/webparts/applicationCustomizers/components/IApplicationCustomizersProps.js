b82ec717b80a854b018c2e1204b03c9294f15331
kunj-sangani <Kunj@1212>
2020-11-07 15:02:41 +0530

    Revert "Added sample react-Edit-Application Customizer"
    This reverts commit 6198eac3bd3be1c8af03a2c2ce1334a431fa6027.

--- samples/react-Edit-ApplicationCustomizers/lib/webparts/applicationCustomizers/components/IApplicationCustomizersProps.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=IApplicationCustomizersProps.js.map
--- samples/react-application-backToTop/src/extensions/backToTop/loc/myStrings.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface IBackToTopApplicationCustomizerStrings {
-  Title: string;
-}
-
-declare module 'BackToTopApplicationCustomizerStrings' {
-  const strings: IBackToTopApplicationCustomizerStrings;
-  export = strings;
-}
